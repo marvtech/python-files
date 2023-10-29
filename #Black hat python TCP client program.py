d61787c790969e6b85a3718ab71a5bf9c3678b91
Marv Tech <armchairtheo@gmail.com>
2023-10-29 08:19:53 -0700

    Add files via upload

---
+++ #Black hat python TCP client program.py
@@ -0,0 +1,21 @@
+#Black hat python TCP client program
+import socket
+
+target_host = "127.0.0.1"
+target_port = 1234
+
+#create socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect the client
+client.connect((target_host, target_port))
+
+#send some data
+client.send(b"GET / HTTP/1.1\r\nHOST: 127.0.0.1\r\n\r\n")
+
+#receive some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
+
