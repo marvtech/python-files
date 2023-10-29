0da3148da4f65da6c309d9b9cce8b29105706af0
Marv Tech <armchairtheo@gmail.com>
2023-10-29 07:38:07 -0700

    Add files via upload

---
+++ things Ive done.py
@@ -0,0 +1,23 @@
+#create a list of things Ive learned in hacking
+programs_Ive_learned = ["kali linux", "parrot os linux", "Ubuntu linux", "Kubuntu linux", "linux Mint",]
+print("Ive learned these programs", programs_Ive_learned)
+virtual_machines_Ive_learned = ["ESXI", "Virtual box", "Workstation player", "Workstation pro",  "Prox mox", "True Nas", "Work station linux"]
+print("Ive learned these virtual machines", virtual_machines_Ive_learned)
+docker_machines_Ive_learned = ["linux servers", "linux OS", "nginx", "apache2", "mysql", "juice shop", "searxng", ]
+print("Ive learned docker", docker_machines_Ive_learned)
+software_Ive_installed = ["type 1 hypervisors", "type2 hypervisors", "linux machines", "windows machines", "apache2 server", "mariah database", "mysql database", "nginx server", "postgresql server"]
+print("software Ive installed", software_Ive_installed)
+software_Ive_installed.insert(10, "python server")
+print("software Ive installed", software_Ive_installed)
+docker_machines_Ive_learned.append("Portainer")
+print("docker machines Ive learned", docker_machines_Ive_learned)
+hardware_Ive_installed = ["ram in desktops", "ram in laptops", "hard drives in desktops", "NVME drives in laptops", "SSD drives in laptops"]
+print("hardware Ive installed", hardware_Ive_installed)
+hardware_Ive_prepared = ["NVME drive wipe", "ssd drive wipe", "micro sd card wipe", "drive and card formatting", "drive partitioning", "NVME drive cloning"]
+print("hardware Ive prepared", hardware_Ive_prepared)
+hardware_Ive_prepared.extend(["thumb drive raspberry pi", "thumb drive ESXI", "thumbdrive windows 10", "thumb drive linux os"])
+print("hardware Ive prepared", hardware_Ive_prepared)
+networking_labs_built = ["unify router", "linksys router", "linksys switch", "windows 2012 server", "windows AD DNS, DHCP", "PFsense firewall, DNS"]
+print("networking labs Ive built", networking_labs_built)
+websites_Ive_built = ["word press reformed evangelist", "word press sentry on duty", "word press torgcraft"]
+print("websites Ive built", websites_Ive_built)
