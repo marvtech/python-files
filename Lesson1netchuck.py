e91de3129b7b3629b6007e7d8a35f8074341efa9
Marv Tech <armchairtheo@gmail.com>
2023-10-29 07:49:41 -0700

    Add files via upload

---
+++ Lesson1netchuck.py
@@ -0,0 +1,16 @@
+print("hello World!!") #print is a function, remember this, we can create our own functions
+# a string is a list of characters with quotes " or ' and is captured in ( )
+print("Iam learning python")
+print("remember to either use single or double quotation marks, keep consistent")
+print("Iam, really not very good at this yet")
+#I am revisiting my first python lessons because I havent kept up with them.
+
+print("""Iam learning
+ remember to be consistent
+  Im not very good at this""")
+#notice the 3 quotations marks beginning and end
+#this is called multi-line_strings
+print("call to me \n" + "I will show you \n" +"great and mighty things")
+#this multi-line-string is separated by backslash \n and a +
+print("Jesus is Lord\n" * 5)
+#this multiplies the string in quotes with line breaks 5 times.
